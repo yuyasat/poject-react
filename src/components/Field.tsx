e39c8978b40fb6d900323a1dcb94b66e6e5ecead
yuyasat <takiyour@gmail.com>
2023-04-02 12:49:37 +0900

    implement Algorithm

--- src/components/Field.tsx
+++ src/components/Field.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useReducer, useState } from 'react'
 import Color from '../modules/Color';
 import GameSetting from '../modules/GameSetting';
 import TopField from './TopField';
@@ -8,6 +8,7 @@ import GridRow from './GridRow';
 import { TopState } from '../Types';
 import { getMovedFirstColumn, getMovedSecondColumn, getRotatedSecondColumn, getRotatedSecondRow } from '../modules/KeyOperation';
 import KeyCode from '../modules/KeyCode';
+import { allocateGrids, countColor, deleteColor, getDropedGridStates, isColumnFilled } from '../modules/GameAlgorithm';
 
 const getInitialColor = (j: number, i: number, topState: any) => {
   if (i === GameSetting.initialColumn && j === GameSetting.initialSecondRow) {
@@ -99,26 +100,40 @@ const getTopGridStates = (topState: TopState) => {
   return topGridStates
 }
 
+
+const styles = () => {
+  return {
+    fieldWrap: {
+      width: `${(GameSetting.column * 30) + 200}px`,
+      height: `${(GameSetting.row * 20) + 140}px`
+    },
+    field: {
+      width: `${(GameSetting.column * 30) + 50}px`
+    }
+  }
+}
+
 const Field = () => {
   const [topState, setTopState] = useState(initialTopState);
   const [topGridStates, setTopGridStates] = useState(initialTopGridStates);
-  const [gridStates, setGreidStates] = useState(initialGridState);
+  const [gridStates, setGridStates] = useState(initialGridState);
   const [nextState, setNextState] = useState(initialNextState);
   const [nextNextState, setNextNextState] = useState(initialNextNextState);
   const [chainCount, setChainCount] = useState(0);
   const [maxChainCount, setMaxChainCount] = useState(0);
   const [keyAccept, setKeyAccept] = useState(true);
 
+
   useEffect(() => {
     if (keyAccept) {
-      window.addEventListener('keydown', handleKeyDown)
+      document.addEventListener('keydown', handleKeyDown)
     } else {
-      window.removeEventListener('keydown', handleKeyDown)
+      document.removeEventListener('keydown', handleKeyDown)
     }
   }, [keyAccept]);
 
   useEffect(() => {
-    window.addEventListener('keydown', handleKeyDown)
+    document.addEventListener('keydown', handleKeyDown)
   }, []);
 
   useEffect(() => {
@@ -129,7 +144,7 @@ const Field = () => {
 
   const handleKeyDown = (e: any) => {
     if (e.keyCode === KeyCode.down) {
-      // this.handleDown()
+      handleDown()
       return
     }
 
@@ -145,7 +160,90 @@ const Field = () => {
     })
   }
 
-  const handleDown = () => { }
+  const handleDown = () => {
+    if (isColumnFilled(gridStates, topState)) { return }
+
+    const _gridStates = getDropedGridStates(gridStates, topState)
+
+    setGridStates(_gridStates);
+    setKeyAccept(false);
+
+    setTimeout(() => {
+      chain(gridStates, 0)
+    }, 200)
+
+    const waitingTopState = Object.assign(initialTopState, {
+      firstColor: nextState.firstColor,
+      secondColor: nextState.secondColor
+    })
+
+    const waitingNextState = {
+      firstColor: nextNextState.firstColor,
+      secondColor: nextNextState.secondColor
+    }
+
+    const waitingNextNextState = {
+      firstColor: Math.floor(Math.random() * 4) + 1,
+      secondColor: Math.floor(Math.random() * 4) + 1
+    }
+
+    setTopGridStates(getTopGridStates(waitingTopState));
+    setTopState(waitingTopState);
+    setNextState(waitingNextState);
+    setNextNextState(waitingNextNextState);
+  }
+
+  const chain = (chainedGridStates: any, chainCount: number) => {
+    const {
+      gridStates, countedChainCount
+    } = getDeletedGridStates(chainedGridStates, chainCount)
+
+    chainCount = countedChainCount
+
+    setTimeout(() => {
+      setGridStates(gridStates)
+
+      setTimeout(() => {
+        dropGrids(gridStates, chainCount)
+      }, 200)
+    }, 200)
+
+    return gridStates
+  }
+
+
+  const getDeletedGridStates = (gridStates: any, chainCount: number) => {
+    let deletedColor = Color.none
+    gridStates.forEach((grids: any, j: number) => {
+      grids.forEach((grid: any, i: number) => {
+        if (grid.color !== Color.none && countColor(j, i, gridStates) >= 4) {
+          if (deletedColor === Color.none || deletedColor === grid.color) {
+            deletedColor = grid.color
+            chainCount++
+          }
+          gridStates = deleteColor(j, i, gridStates)
+          setChainCount(chainCount);
+          if (maxChainCount < chainCount) {
+            setMaxChainCount(chainCount);
+          }
+        }
+      })
+    })
+
+    return { gridStates, countedChainCount: chainCount }
+  }
+
+  const dropGrids = (deletedGridStates: any, chainCount: number) => {
+    const { count, gridStates } = allocateGrids(deletedGridStates)
+
+    setGridStates(gridStates);
+
+    if (count > 0) {
+      setTimeout(() => { chain(gridStates, chainCount) }, 200)
+    } else {
+      setKeyAccept(true);
+    }
+  }
 
   const grids = gridStates.map((gridStateRow: any, j: number) => {
     return (
@@ -158,8 +256,8 @@ const Field = () => {
 
   return (
     <div>
-      <div className='FieldWrap'>
-        <div className='Field'>
+      <div className='FieldWrap' style={styles().fieldWrap}>
+        <div className='Field' style={styles().field}>
           <TopField
             topState={topState}
             topGridStates={topGridStates}
